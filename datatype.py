09ced99aa17bcf253b032f36a94447a762afe6f2
rahilvadiya <155240534+rahilvadiya@users.noreply.github.com>
2024-06-22 10:28:15 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,32 @@
+#by default pythone provide following bilt in data type
+
+# int = 3,-8,0,etc.
+# float = 7.329, -9.3, 0.0000001
+# complex = 6+2i
+
+a = 3                 #int
+f = 0.00000001        #float
+c = complex(8, 3)     #complexff
+
+print(a)
+print(f)
+print(c)
+print(type(c))
+
+#list ,tuple
+
+#example of list
+
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+
+#example of tuple
+
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+
+#dictionary
+
+dict1={"name":"rahil","age":19,"canVote":True}
+print(dict1)
+
